dd45e42fef27ee399d72b0b10a3f30b25a3a027b
Chris Walker <chris@thechriswalker.net>
2020-05-22 21:29:15 +0100

    Initial Server Timing code

---
+++ webapp/helmet/README.md
@@ -0,0 +1,16 @@
+# helmet is basic security headers for your web app
+
+Idea (and name) is totally taken from https://github.com/helmetjs, which I have used in Koa and is a great little
+middleware.
+
+so far I have implemented:
+
+- [x] frameguard
+- [x] dns prefetch control
+- [x] no sniff (mime types)
+- [ ] csp
+- [ ] expect CT (certificate transparency
+- [ ] feature policy
+- [ ] hsts
+- [ ] referrer policy
+- [ ]
--- webapp/server_timing.ts
+++ webapp/server_timing.ts
@@ -1,5 +1,56 @@
-export type ServerTiming = {
+import { deferred } from "https://deno.land/std/async/mod.ts";
+
+export type ServerTimingMetric = {
   // metric name, required
   metric: string;
-  durationMs: number;
+  durationMs?: number;
 };
+
+export class ServerTiming {
+  #metrics: ServerTimingMetric[] = [];
+  #pending: Promise<unknown>[] = [];
+
+  public log(metric: string) {
+    this.#metrics.push({ metric });
+  }
+
+  public time(metric: string) {
+    const pending = deferred();
+    this.#pending.push(pending);
+    const start = Date.now();
+    let done = false;
+    return () => {
+      if (done) return;
+      done = true;
+      this.#metrics.push({ metric, durationMs: Date.now() - start });
+      this.#pending.splice(this.#pending.indexOf(pending), 1);
+      pending.resolve();
+    };
+  }
+
+  public setHeaders(headers: Headers) {
+    this.writeToHeaders(headers);
+    if (this.#pending.length) {
+      headers.append("trailers", "server-timing");
+    }
+  }
+
+  public setTrailers(headers: Headers) {
+    if (this.#pending.length) {
+      throw new Error("pending server timings are not yet resolved");
+    }
+    this.writeToHeaders(headers);
+  }
+
+  private writeToHeaders(headers: Headers) {
+    const metrics = this.#metrics.slice();
+    this.#metrics.length = 0;
+    metrics.forEach(({ metric, durationMs }) => {
+      let dur =
+        durationMs === undefined
+          ? ""
+          : `;dur=${(durationMs / 1000).toFixed(3).replace(/.?0+$/, "")}`;
+      headers.append("server-timing", `${metric}${dur}`);
+    });
+  }
+}
